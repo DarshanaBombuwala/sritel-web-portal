30dd9e8009afc7c9a5eaefd29c744ffce0224a40
DarshanaBombuwala <navodhya123@gmail.com>
2024-09-20 14:23:08 +0530

    Created Authorization for user-service

--- services/user-service/src/main/java/com/sritel/user_service/service/UserAccountService.java
+++ services/user-service/src/main/java/com/sritel/user_service/service/UserAccountService.java
@@ -1,7 +1,17 @@
 package com.sritel.user_service.service;
 
+import com.sritel.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserAccountService {
+
+    private final UserRepository userRepository;
+
+
+    public boolean changePassword(String currentPassword, String newPassword) {
+
+    }
 }
