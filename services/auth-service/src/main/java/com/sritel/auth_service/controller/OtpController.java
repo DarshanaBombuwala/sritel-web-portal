ac669d5c2e76abd664a822d4813d15911c590b3b
DarshanaBombuwala <navodhya123@gmail.com>
2024-09-23 02:09:36 +0530

    Setup twillio for otp verfication

--- services/auth-service/src/main/java/com/sritel/auth_service/controller/OtpController.java
+++ services/auth-service/src/main/java/com/sritel/auth_service/controller/OtpController.java
@@ -2,12 +2,19 @@ package com.sritel.auth_service.controller;
 import com.sritel.auth_service.dto.OtpVerificationDto;
 import com.sritel.auth_service.service.OtpService;
 import com.sritel.auth_service.dto.OtpRequestDto;
+import com.twilio.Twilio;
+import com.twilio.rest.verify.v2.service.Verification;
+import com.twilio.rest.verify.v2.service.VerificationCheck;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.twilio.example.ValidationExample.ACCOUNT_SID;
+import static com.twilio.example.ValidationExample.AUTH_TOKEN;
 
 @RestController
 @RequestMapping("otp")
@@ -17,14 +24,35 @@ public class OtpController {
     private final OtpService otpService;
 
     @PostMapping("/send-otp")
-    public ResponseEntity<Void> sendOtp(@RequestBody OtpRequestDto otpRequest){
-        otpService.sendOtp(otpRequest.getMobileNumber());
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> sendOtp(@RequestBody OtpRequestDto otpRequest){
+        Twilio.init(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_TOKEN"));
+
+        Verification verification = Verification.creator(
+                        "VAe545522581b54399c3d2d4695d878fb5", // this is your verification sid
+                        "+94788886171", //this is your Twilio verified recipient phone number
+                        "sms") // this is your channel type
+                .create();
+
+        return new ResponseEntity<>("Your OTP has been sent to your verified phone number", HttpStatus.OK);
     }
 
     @PostMapping("/verify-otp")
-    public ResponseEntity<Boolean> verifyOtp(@RequestBody OtpVerificationDto otpVerification) {
-        boolean isValid = otpService.verifyOtp(otpVerification.getMobileNumber(), otpVerification.getOtp());
-        return ResponseEntity.ok(isValid);
+    public ResponseEntity<String> verifyOtp(@RequestBody OtpVerificationDto otpVerification) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        try {
+
+            VerificationCheck verificationCheck = VerificationCheck.creator(
+                            "VAe545522581b54399c3d2d4695d878fb5")
+                    .setTo("+94788886171")
+                    .setCode("486578")
+                    .create();
+
+            System.out.println(verificationCheck.getStatus());
+
+        } catch (Exception e) {
+            return new ResponseEntity<>("Verification failed.", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("This user's verification has been completed successfully", HttpStatus.OK);
     }
 }
