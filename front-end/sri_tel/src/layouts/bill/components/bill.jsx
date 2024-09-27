4b8c96501c5f78212b0b34082dc68fe867987983
retro1000 <damithajayawardana678@gmail.com>
2024-09-27 23:26:53 +0530

    "Updated BillsPage component to fetch data from API, added createdDate to Bill component, and removed .DS_Store from .gitignore"

--- front-end/sri_tel/src/layouts/bill/components/bill.jsx
+++ front-end/sri_tel/src/layouts/bill/components/bill.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Bill = ({ description, amount }) => {
+const Bill = ({ description, amount, createdDate }) => {
   return (
     <div>
+      <p><strong>Created Date:</strong> {createdDate}</p>
       <p><strong>Description:</strong> {description}</p>
       <p><strong>Amount:</strong> ${amount}</p>
       <hr />
