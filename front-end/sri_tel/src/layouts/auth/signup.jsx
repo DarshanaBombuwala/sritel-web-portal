19bca8ed4e2d5d9f91ceca2037f745e12224f44a
retro1000 <136360020+retro1000@users.noreply.github.com>
2024-09-27 21:49:11 +0530

    Update signup.jsx

--- front-end/sri_tel/src/layouts/auth/signup.jsx
+++ front-end/sri_tel/src/layouts/auth/signup.jsx
@@ -4,12 +4,17 @@ import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
   const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [OTP, setOTP] = useState('');
+  const [successOTP, setSuccessOTP] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
     
     if (password !== confirmPassword) {
@@ -17,25 +22,75 @@ const SignUp = () => {
       return;
     }
 
-    console.log("User signed up:", { username, email, password });
-    axios.post('http://localhost:8081/AUTH-SERVICE/authentication/login', {username: username, password: password})
+    axios.post('http://localhost:8081/USER-SERVICE/user/getOtp', {username: mobileNumber})
       .then(response => {
-        if(response.status===200){
-          navigate('/options')
-        }
+        
       })
       .catch(error => {
         
       })
 
 
-    navigate('/signin');
+  };
+
+  const handleOtp = async(e) => {
+    e.preventDefault();
+    
+    axios.post('http://localhost:8081/USER-SERVICE/user/verifyOtp', {otpCode: OTP})
+      .then(response => {
+       if(response.status===200) handleRegister()
+      })
+      .catch(error => {
+        
+      })
+  };
+
+  const handleRegister = async(e) => {
+    e.preventDefault();
+    
+    axios.post('http://localhost:8081/USER-SERVICE/user/register', {userName: username, password: password, mobileNumber: mobileNumber, firstName: firstName, lastName: lastName, email: email})
+      .then(response => {
+       if(response.status===200) navigate('/options')
+      })
+      .catch(error => {
+        
+      })
   };
 
   return (
     <div>
       <h2>Sign Up</h2>
       <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First name:</label>
+          <input
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last name:</label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
         <div>
           <label htmlFor="username">Username:</label>
           <input
@@ -47,12 +102,12 @@ const SignUp = () => {
           />
         </div>
         <div>
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email">Mobile number:</label>
           <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            id="mobileNumberl"
+            value={mobileNumber}
+            onChange={(e) => setMobileNumber(e.target.value)}
             required
           />
         </div>
@@ -77,6 +132,21 @@ const SignUp = () => {
           />
         </div>
         <button type="submit">Sign Up</button>
+      </form>
+      <br></br>
+      <form onSubmit={handleOtp}>
+        <div>
+          <br />
+          <label>OTP: </label>
+          <input
+            type="text"
+            value={OTP}
+            onChange={(e) => setOTP(e.target.value)}
+            required
+          />
+          <button type="submit">Submit</button>
+        </div>
+          {successOTP}
       </form>
     </div>
   );
