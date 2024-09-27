abc88b7e94c113198c6ff0bde68e6a6aa2dba9bf
retro1000 <136360020+retro1000@users.noreply.github.com>
2024-09-28 00:50:38 +0530

    Update service.jsx

--- front-end/sri_tel/src/layouts/servicesubcription/components/service.jsx
+++ front-end/sri_tel/src/layouts/servicesubcription/components/service.jsx
@@ -1,11 +1,24 @@
+import axios from 'axios';
 import React from 'react';
 
-const Service = ({ name, description, cost }) => {
+
+const Service = ({ sub, key, name, description, cost }) => {
+
+  
+  const subscribe = async () => {
+    await axios.post(`http://localhost:8081/SUBSCRPTION_SERVICE/api/subscription/${sub?'deactivate':'activate'}/${key}`)
+      .then(response => {
+
+      })
+      .catch(error => {
+
+      })
+  }
   return (
     <div>
       <h3>{name}</h3>
       <p>{description} - {cost}</p>
-      <button>subscribe</button>
+      <button onClick={subscribe}>{sub?'Unsubscribe':'Subscribe'}</button>
     </div>
   );
 };
