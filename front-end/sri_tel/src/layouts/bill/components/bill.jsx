00a9c009c505f415e455aa717c55412973ac09f8
retro1000 <136360020+retro1000@users.noreply.github.com>
2024-09-28 00:51:17 +0530

    Update bill.jsx

--- front-end/sri_tel/src/layouts/bill/components/bill.jsx
+++ front-end/sri_tel/src/layouts/bill/components/bill.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Bill = ({ description, amount, createdDate }) => {
+const Bill = ({ key, description, amount, createdDate, pay }) => {
+
+  const navigate = useNavigate()
+
+  const pay = () => {
+    navigate('/payments', {state: { id: key }})
+  }
+
   return (
     <div>
       <p><strong>Created Date:</strong> {createdDate}</p>
       <p><strong>Description:</strong> {description}</p>
       <p><strong>Amount:</strong> ${amount}</p>
+      {pay && <button onClick={pay}>Pay Now</button>}
       <hr />
     </div>
   );
