71447194c4451f1a350475c121ce08ba301591b5
iamluwin <luwin.anuka@gmail.com>
2024-09-28 21:24:25 +0530

    navbar styles

--- front-end/sri_tel/src/layouts/navbar/navbar.jsx
+++ front-end/sri_tel/src/layouts/navbar/navbar.jsx
@@ -1,19 +1,18 @@
 import { Link } from "react-router-dom";
+import '../../App.css';
 
 const Navbar = () => {
     return(
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/signin">Sign In</Link></li>
-            <li><Link to="/signup">Sign up</Link></li>
-            <li><Link to="/options">options</Link></li>
-            <li><Link to="/customercare">customer care</Link></li>
-
+        <nav className="navbar">
+          <ul className="navbar-list">
+            <li className="navbar-item"><Link className="navbar-link" to="/">Home</Link></li>
+            <li className="navbar-item"><Link className="navbar-link" to="/signin">Sign In</Link></li>
+            <li className="navbar-item"><Link className="navbar-link" to="/signup">Sign up</Link></li>
+            <li className="navbar-item"><Link className="navbar-link" to="/options">Options</Link></li>
+            <li className="navbar-item"><Link className="navbar-link" to="/customercare">Customer Care</Link></li>
           </ul>
         </nav>
     );
 }
 
 export default Navbar;
-
